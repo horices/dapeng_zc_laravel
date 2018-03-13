fc03f202b8cae02fe42cba8a8d2e3e017af6ddff
soen <970852318@qq.com>
2018-03-13 17:56:48 +0800

    --soen

--- app/Models/UserRegistrationModel.php
+++ app/Models/UserRegistrationModel.php
@@ -22,16 +22,71 @@ class UserRegistrationModel extends BaseModel{
         'MYFQ'      =>  '蚂蚁分期',
     ];
 
+    /**
+     * 新增数据
+     * @param $data
+     * @return mixed
+     */
     function addData($data){
         $validator = Validator::make($data, [
-            'mobile'=>'required|regex:/\d{11}/|unique:user_registration',
-            'name' => 'required|unique:posts|max:255',
-            'qq' => 'required',
+            'mobile'            =>'required|regex:/\d{11}/|unique:user_registration',
+            'name'              => 'required|unique:user_registration|max:255',
+            'qq'                => 'required|regex:/\d{5,11}/|unique:user_registration',
+            'package_id'        => 'exists:course_package,id',
+            'package_attach_id' => 'exists:course_package,id',
+            'rebate_id'         => 'exists:rebate_activity,id',
+            'amount_submitted'  =>  'required|numeric'
         ],[
-            'mobile.required' =>  '请输入正确的学员手机号！',
-            'mobile.unique'   =>  '该学员手机号已存在！',
-            'name.required'   =>  '请输入学员姓名！',
-            'qq.required'     =>  '请输入学员QQ号！',
+            'mobile.required'   =>  '请输入正确的学员手机号！',
+            'mobile.unique'     =>  '该学员手机号已存在！',
+            'name.required'     =>  '请输入学员姓名！',
+            'qq.required'       =>  '请输入学员QQ号！',
+            'qq.regex'          =>  '学员QQ号格式错误！',
+            'qq.unique'         =>  '学员QQ号已存在！',
+            'package_id.exists' =>  '请选择正确的课程主套餐！',
+            'package_attach_id.exists' =>  '请选择正确的课程副套餐！',
+            'rebate_id.exists'  =>  '请选择正确的优惠活动！',
+            'amount_submitted,required'=>   '已提交金额有误！',
+            'amount_submitted,numeric'=>   '已提交金额有误！',
         ]);
+        //执行验证
+        $validator->validate();
+        return self::create($data);
     }
+
+    /**
+     * 更新数据
+     * @param $data
+     * @return bool
+     */
+    function updateData($data){
+        $validator = Validator::make($data, [
+            'id'                =>  'required|numeric',
+            'mobile'            =>'sometimes|required|regex:/\d{11}/|unique:user_registration',
+            'name'              => 'sometimes|required|unique:user_registration|max:255',
+            'qq'                => 'sometimes|required|regex:/\d{5,11}/|unique:user_registration',
+            'package_id'        => 'sometimes|exists:course_package,id',
+            'package_attach_id' => 'sometimes|exists:course_package,id',
+            'rebate_id'         => 'sometimes|exists:rebate_activity,id',
+            'amount_submitted'  =>  'sometimes|required|numeric'
+        ],[
+            'id.required'       =>  '更新的支付记录错误！',
+            'id.numeric'        =>  '更新的支付记录错误！',
+            'mobile.required'   =>  '请输入正确的学员手机号！',
+            'mobile.unique'     =>  '该学员手机号已存在！',
+            'name.required'     =>  '请输入学员姓名！',
+            'qq.required'       =>  '请输入学员QQ号！',
+            'qq.regex'          =>  '学员QQ号格式错误！',
+            'qq.unique'         =>  '学员QQ号已存在！',
+            'package_id.exists' =>  '请选择正确的课程主套餐！',
+            'package_attach_id.exists' =>  '请选择正确的课程副套餐！',
+            'rebate_id.exists'  =>  '请选择正确的优惠活动！',
+            'amount_submitted,required'=>   '已提交金额有误！',
+            'amount_submitted,numeric'=>   '已提交金额有误！',
+        ]);
+        $validator->validate();
+        self::find($data);
+        return self::save($data['id']);
+    }
+
 }
