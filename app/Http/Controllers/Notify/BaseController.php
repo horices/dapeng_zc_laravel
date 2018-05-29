43bcc182329206bdc971bc4360195b4eb7db1851
horice <864742615@qq.com>
2018-05-29 12:02:04 +0800

    通知时，记录参数

--- app/Http/Controllers/Notify/BaseController.php
+++ app/Http/Controllers/Notify/BaseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Notify;
 
 use App\Http\Controllers\BaseController as Controller;
 use Curl\Curl;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,11 @@ class BaseController extends Controller
         /**
          * 当前学院没设计学院时，需要对该通知进行转发
          */
-        /*dd(route(Route::currentRouteName(),['a'=>1],false));
-        dd(Request::fullUrl());
-        dd($curl->post()->response);*/
+        //记录通知
+        //记录请求的所有数据
+        Log::info("请求地址:".url()->full());
+        Log::info("请求参数:");
+        Log::info(Request::all());
 
     }
 }
