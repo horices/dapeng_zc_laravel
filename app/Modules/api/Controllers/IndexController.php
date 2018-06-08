d56b5c1f86d8252b5427078656b2da76d3640087
horice <864742615@qq.com>
2018-06-08 11:10:04 +0800

    删除Modules 文件夹

--- app/Modules/api/Controllers/IndexController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Modules\api\Controllers;
-use App\Http\Controllers\BaseController;
-
-class IndexController extends BaseController{
-    function index(){
-        return "adsadfasdf";
-    }
-}
