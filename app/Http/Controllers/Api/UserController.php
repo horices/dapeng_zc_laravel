4c9406adfaea84a76b1a890fe8786e3901f4efe8
horice <864742615@qq.com>
2018-06-27 15:25:32 +0800

    设计学院添加学院参数

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -20,15 +20,21 @@ class UserController extends BaseController
      * @return \App\Models\UserModel|void
      */
     function postLogin(Request $request){
+
         Validator::make($request->all(),[
+            'schoolId'  =>  'required|in:'.Util::SCHOOL_NAME_MS.",".Util::SCHOOL_NAME_SJ,
             'mobile'    =>  'required|string|size:11',
             'password'  =>  'required'
         ],[
+            'schoolId.required' =>  "请输入学院名称",
+            'schoolId.in'       =>  '请输入正常的学院名称',
             'mobile.required'   =>  "请输入手机号",
             'mobile.size'       =>  "请输入11位的手机号",
             'password.required' =>  "请输入密码"
         ])->validate();
-
+        if(Util::getSchoolName() == Util::SCHOOL_NAME_SJ && $request->get("schoolId") == Util::SCHOOL_NAME_MS){
+            return $this->forward();
+        }
         $user = UserModel::where([
             'mobile'    => $request->get("mobile"),
             'password'  => md5($request->get("password"))
